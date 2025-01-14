267d9cbb5a0170360389a50d33a7d166b139735b
LearnerAnisha <anishagautam012@gmail.com>
2025-01-14 08:25:35 +0545

    added getter methods to student class

--- SchoolManagementSystem/src/schoolManagementSystem/Student.java
+++ SchoolManagementSystem/src/schoolManagementSystem/Student.java
@@ -2,12 +2,24 @@ package schoolManagementSystem;
 
 public class Student {
 	// attributes of class student
-	String name;
-	int age;
+	private String name;
+	private int age;
 	
 	//constructor
 	public Student(String name,int age) {
 		this.name = name;
 		this.age = age;
 	}
+	
+	// getter and setter for variable name
+	public String getName() {
+		return name;
+	}
+
+	
+	// getter and setter for variable age
+	public int getAge() {
+		return age;
+	}
+
 }
