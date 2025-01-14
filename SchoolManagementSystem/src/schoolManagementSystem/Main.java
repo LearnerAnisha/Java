705b7cd6606160fecc6812380a75500fe3bcf013
LearnerAnisha <anishagautam012@gmail.com>
2025-01-14 08:22:01 +0545

    add attributes and constructor

---
+++ SchoolManagementSystem/src/schoolManagementSystem/Main.java
@@ -0,0 +1,5 @@
+package schoolManagementSystem;
+
+public class Main {
+
+}
---
+++ SchoolManagementSystem/src/schoolManagementSystem/Student.java
@@ -0,0 +1,13 @@
+package schoolManagementSystem;
+
+public class Student {
+	// attributes of class student
+	String name;
+	int age;
+	
+	//constructor
+	public Student(String name,int age) {
+		this.name = name;
+		this.age = age;
+	}
+}
